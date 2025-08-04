6fca06cf0ee836bdf41a5c7393d91a123fee7b83
Luu Truong Toan <luutruongtoanltt@gmail.com>
2025-08-04 22:45:56 +0700

    setup x3

--- src/app.js
+++ src/app.js
@@ -1,44 +1,3 @@
-// // app.js
-// const express = require("express");
-// const cors = require("cors");
-// const userRoutes = require("./routes/user.routes");
-// const authRoutes = require("./routes/auth.routes");
-// const bookingRoutes = require("./routes/booking.routes");
-
-// const app = express();
-
-// // Cho phép frontend ở localhost:5173 truy cập
-// // app.use(cors({ origin: "http://localhost:5173" }));
-// const allowedOrigins = [
-//     'http://localhost:5173',
-//     'https://duong123321.netlify.app'
-// ];
-
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     credentials: true
-// }));
-
-// // Xử lý preflight request (OPTIONS)
-// app.options('*', cors());
-
-// // Phân tích JSON body
-// app.use(express.json());
-
-// // Các route
-// app.use("/api/users", userRoutes);
-// app.use("/api/auth", authRoutes);
-
-// // Mount bookingRoutes dưới path /api/bookings
-// app.use("/api/bookings", bookingRoutes);
-
-// module.exports = app;
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
@@ -47,29 +6,20 @@ const bookingRoutes = require("./routes/booking.routes");
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://duong123321.netlify.app'
-];
-
+// Cấu hình CORS cho frontend Netlify và localhost
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [
+    'http://localhost:5173',
+    'https://duong123321.netlify.app'
+  ],
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 // Apply CORS globally
 app.use(cors(corsOptions));
-
-// Apply to all OPTIONS requests
-app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions)); // OPTIONS cho preflight request
 
 // Body parser
 app.use(express.json());
